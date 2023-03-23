ba6890449656639cf46d226d6975b70ff48fa9e7
IngAlanRG <ingalanrosasgarcia@gmail.com>
2023-03-23 14:13:13 -0600

    Ejercicio 7

---
+++ Ejercicio7.js
@@ -0,0 +1,321 @@
+arreglo = [
+    {
+        "_id": "641cb004b47ab24391bee71b",
+        "index": 0,
+        "guid": "452682c5-e37f-41a4-8163-72cb00700389",
+        "isActive": false,
+        "balance": "$3,215.92",
+        "picture": "http://placehold.it/32x32",
+        "age": 35,
+        "eyeColor": "blue",
+        "name": "Nannie Peters",
+        "gender": "female",
+        "company": "SURETECH",
+        "email": "nanniepeters@suretech.com",
+        "phone": "+1 (892) 514-3096",
+        "address": "372 Will Place, Maury, Colorado, 6033",
+        "about": "Laborum laboris voluptate ullamco adipisicing ex reprehenderit cillum elit id est sint nostrud laboris. Tempor tempor anim cillum non commodo veniam aute id dolor tempor est. Occaecat mollit ex in aute laborum laborum sit dolor Lorem sit irure. Quis dolore ex incididunt nulla non id commodo dolor voluptate ullamco sunt excepteur. Minim tempor ex ut nulla sit Lorem ullamco labore cillum. Labore ea consequat labore sint elit reprehenderit qui velit ex sit.\r\n",
+        "registered": "2017-11-18T05:28:36 +06:00",
+        "latitude": -0.218033,
+        "longitude": 102.760014,
+        "tags": [
+            "excepteur",
+            "voluptate",
+            "velit",
+            "nisi",
+            "laborum",
+            "irure",
+            "duis"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Bond Olson"
+            },
+            {
+                "id": 1,
+                "name": "Frank Day"
+            },
+            {
+                "id": 2,
+                "name": "Obrien Fletcher"
+            }
+        ],
+        "greeting": "Hello, Nannie Peters! You have 5 unread messages.",
+        "favoriteFruit": "apple"
+    },
+    {
+        "_id": "641cb004ee12d4cf212d2f99",
+        "index": 1,
+        "guid": "1125ef82-3098-4fd3-8e3e-ea15afff1767",
+        "isActive": false,
+        "balance": "$3,361.23",
+        "picture": "http://placehold.it/32x32",
+        "age": 31,
+        "eyeColor": "blue",
+        "name": "Brady Clark",
+        "gender": "male",
+        "company": "ZOINAGE",
+        "email": "bradyclark@zoinage.com",
+        "phone": "+1 (850) 558-3749",
+        "address": "103 Varick Avenue, Chesapeake, Guam, 4570",
+        "about": "Anim et sunt cillum labore aute. Eiusmod nisi culpa ipsum id sint. Enim pariatur ullamco ut aliqua. Qui ex ullamco officia amet veniam ipsum minim Lorem excepteur pariatur. Dolore culpa nulla cillum excepteur deserunt nulla commodo Lorem.\r\n",
+        "registered": "2020-09-13T09:06:37 +05:00",
+        "latitude": 39.613065,
+        "longitude": -155.094122,
+        "tags": [
+            "culpa",
+            "anim",
+            "tempor",
+            "pariatur",
+            "esse",
+            "nisi",
+            "tempor"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Aurelia Gallegos"
+            },
+            {
+                "id": 1,
+                "name": "Hays Conner"
+            },
+            {
+                "id": 2,
+                "name": "Valdez Hinton"
+            }
+        ],
+        "greeting": "Hello, Brady Clark! You have 3 unread messages.",
+        "favoriteFruit": "banana"
+    },
+    {
+        "_id": "641cb0045f23eb7d725c5a7b",
+        "index": 2,
+        "guid": "0c71b35d-4070-4033-b019-ba75758c4794",
+        "isActive": true,
+        "balance": "$1,299.97",
+        "picture": "http://placehold.it/32x32",
+        "age": 20,
+        "eyeColor": "green",
+        "name": "Guzman Barnes",
+        "gender": "male",
+        "company": "REMOLD",
+        "email": "guzmanbarnes@remold.com",
+        "phone": "+1 (972) 553-2038",
+        "address": "223 Forest Place, Carlos, Missouri, 2507",
+        "about": "Id sit non occaecat ut ut ullamco non proident ea. Veniam labore consectetur dolor consequat commodo. Reprehenderit non deserunt aute mollit duis labore cupidatat voluptate consectetur. Labore quis fugiat minim non. Anim nostrud qui qui est.\r\n",
+        "registered": "2016-05-11T07:11:10 +05:00",
+        "latitude": 38.676458,
+        "longitude": 5.771575,
+        "tags": [
+            "minim",
+            "fugiat",
+            "consequat",
+            "laboris",
+            "reprehenderit",
+            "eu",
+            "ea"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Edith Dyer"
+            },
+            {
+                "id": 1,
+                "name": "Patti Garza"
+            },
+            {
+                "id": 2,
+                "name": "Rivera Johns"
+            }
+        ],
+        "greeting": "Hello, Guzman Barnes! You have 1 unread messages.",
+        "favoriteFruit": "banana"
+    },
+    {
+        "_id": "641cb004471eee67270715bc",
+        "index": 3,
+        "guid": "58c908a0-d629-495c-a0ff-9399db24907c",
+        "isActive": false,
+        "balance": "$1,421.65",
+        "picture": "http://placehold.it/32x32",
+        "age": 32,
+        "eyeColor": "blue",
+        "name": "Howell Ochoa",
+        "gender": "male",
+        "company": "TSUNAMIA",
+        "email": "howellochoa@tsunamia.com",
+        "phone": "+1 (982) 433-2166",
+        "address": "450 Glenwood Road, Rose, Indiana, 8335",
+        "about": "Ea eiusmod do cupidatat pariatur consequat irure commodo ullamco. Fugiat mollit est proident nulla culpa ipsum enim pariatur aliqua. Lorem quis tempor ex proident est ad dolor eiusmod ullamco laborum anim excepteur deserunt. Ut aute ipsum ex irure dolore Lorem ea ex in culpa mollit.\r\n",
+        "registered": "2016-01-18T04:52:59 +06:00",
+        "latitude": 29.292602,
+        "longitude": 131.994694,
+        "tags": [
+            "incididunt",
+            "est",
+            "enim",
+            "excepteur",
+            "mollit",
+            "esse",
+            "elit"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Deidre Fields"
+            },
+            {
+                "id": 1,
+                "name": "Emerson Brown"
+            },
+            {
+                "id": 2,
+                "name": "Fry Nicholson"
+            }
+        ],
+        "greeting": "Hello, Howell Ochoa! You have 10 unread messages.",
+        "favoriteFruit": "strawberry"
+    },
+    {
+        "_id": "641cb0044656dce6852b5900",
+        "index": 4,
+        "guid": "99e2813f-11db-41d1-bf9a-de8ad2380aab",
+        "isActive": true,
+        "balance": "$3,708.78",
+        "picture": "http://placehold.it/32x32",
+        "age": 23,
+        "eyeColor": "blue",
+        "name": "Lynda Larson",
+        "gender": "female",
+        "company": "UPDAT",
+        "email": "lyndalarson@updat.com",
+        "phone": "+1 (938) 445-3295",
+        "address": "535 Dodworth Street, Glidden, District Of Columbia, 7346",
+        "about": "Magna reprehenderit ex duis voluptate reprehenderit non tempor consequat enim reprehenderit. Id nostrud ullamco mollit sit Lorem consectetur veniam esse ex excepteur. In minim nostrud mollit sit pariatur pariatur ullamco eu. Lorem adipisicing esse enim ullamco nulla ea cillum eu culpa mollit culpa duis amet in. Reprehenderit cillum amet in culpa aliqua id sit mollit est dolore dolore aute aliqua.\r\n",
+        "registered": "2020-03-12T11:53:30 +06:00",
+        "latitude": 69.677509,
+        "longitude": -110.657496,
+        "tags": [
+            "laboris",
+            "in",
+            "id",
+            "excepteur",
+            "adipisicing",
+            "aliqua",
+            "deserunt"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "England Trujillo"
+            },
+            {
+                "id": 1,
+                "name": "Carmella Knight"
+            },
+            {
+                "id": 2,
+                "name": "Maryellen Ward"
+            }
+        ],
+        "greeting": "Hello, Lynda Larson! You have 5 unread messages.",
+        "favoriteFruit": "banana"
+    },
+    {
+        "_id": "641cb00430ef5ada10eef212",
+        "index": 5,
+        "guid": "9cfbff01-7c13-41fb-93c3-bf0164eeb461",
+        "isActive": true,
+        "balance": "$1,111.89",
+        "picture": "http://placehold.it/32x32",
+        "age": 37,
+        "eyeColor": "brown",
+        "name": "Claudine Carney",
+        "gender": "female",
+        "company": "VIRXO",
+        "email": "claudinecarney@virxo.com",
+        "phone": "+1 (907) 546-2454",
+        "address": "640 Plaza Street, Blandburg, Kentucky, 2706",
+        "about": "Officia et laborum magna minim irure labore ut esse dolor officia. Esse eiusmod anim cillum amet consequat adipisicing commodo voluptate aliqua. Duis minim amet reprehenderit anim duis ex pariatur. Cupidatat commodo ea est ut non ullamco aute id tempor cupidatat. Consequat mollit fugiat cupidatat veniam adipisicing ad enim adipisicing Lorem voluptate. Consequat incididunt amet officia ut ea sint. Duis incididunt exercitation consectetur Lorem ullamco dolore Lorem culpa dolor consectetur mollit.\r\n",
+        "registered": "2015-03-12T09:26:02 +06:00",
+        "latitude": -81.114003,
+        "longitude": 102.81662,
+        "tags": [
+            "enim",
+            "aliqua",
+            "do",
+            "sunt",
+            "aliquip",
+            "ex",
+            "tempor"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Best Hewitt"
+            },
+            {
+                "id": 1,
+                "name": "Hardin Sharpe"
+            },
+            {
+                "id": 2,
+                "name": "Ingrid Santos"
+            }
+        ],
+        "greeting": "Hello, Claudine Carney! You have 8 unread messages.",
+        "favoriteFruit": "strawberry"
+    },
+    {
+        "_id": "641cb004051ea9438ddadd1b",
+        "index": 6,
+        "guid": "6a66bd7c-6a3e-4e11-ae76-d248bb327176",
+        "isActive": true,
+        "balance": "$3,223.63",
+        "picture": "http://placehold.it/32x32",
+        "age": 26,
+        "eyeColor": "brown",
+        "name": "Boyer Holt",
+        "gender": "male",
+        "company": "MUSAPHICS",
+        "email": "boyerholt@musaphics.com",
+        "phone": "+1 (838) 444-3962",
+        "address": "441 Hull Street, Virgie, Maine, 8228",
+        "about": "Duis id officia enim commodo irure sunt do. Sunt reprehenderit ut qui dolor nulla do esse culpa voluptate proident. Qui aliqua aliqua culpa ut esse enim occaecat laboris mollit laborum tempor proident. Tempor elit ut sit ex voluptate dolor voluptate cillum excepteur aliquip velit officia occaecat eu. Quis laborum excepteur occaecat commodo laborum consequat ullamco enim reprehenderit anim laborum. Et culpa mollit excepteur pariatur ad consequat laboris est laboris pariatur.\r\n",
+        "registered": "2015-12-12T12:46:36 +06:00",
+        "latitude": 12.699067,
+        "longitude": -61.826337,
+        "tags": [
+            "sunt",
+            "deserunt",
+            "Lorem",
+            "est",
+            "non",
+            "Lorem",
+            "occaecat"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Holcomb Huff"
+            },
+            {
+                "id": 1,
+                "name": "Buckley Hampton"
+            },
+            {
+                "id": 2,
+                "name": "Margie Norton"
+            }
+        ],
+        "greeting": "Hello, Boyer Holt! You have 8 unread messages.",
+        "favoriteFruit": "apple"
+    }
+]
+
+arreglo.map((hola) => {
+    console.log(hola);
+})
